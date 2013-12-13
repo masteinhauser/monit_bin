b3a84b8feda0ca4123a1b1eeea2188b4115cd64f
Myles Steinhauser <myles.steinhauser@stratus.com>
2013-12-13 11:26:30 -0500

    Remove worthless tests

--- test/integration/default/rspec/spec/localhost/monit_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe service('monit') do
-  it { should be_running   }
-end
--- test/integration/smartos_inittab/rspec/spec/localhost/monit_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe command('pgrep monit') do
-  it { should return_exit_status 0  }
-end
